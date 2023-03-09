3fc11214c484dac734dec5e1badbd33334e3387b
h4rpy-eagle <hamid@de-fitech.com>
2023-03-09 15:16:52 +0330

    doc: add some comments for options

--- option.go
+++ option.go
@@ -24,8 +24,8 @@ func OptionWithDefaultGenerator(t any) Option {
 // OptionWithFitnessFunc takes a function to calculate chance of the solution to be selected during crossover.
 // Cost and score will be defined separately (Generic solution) based on how the individual is calculated and gets score
 /*
- */
-// TODO add body
+	TODO add example
+*/
 func OptionWithFitnessFunc(fn FitnessFunc) Option {
 	return func(g *ga) error {
 		if fn == nil {
@@ -37,6 +37,9 @@ func OptionWithFitnessFunc(fn FitnessFunc) Option {
 }
 
 // OptionWithPopulationFunc define operations such as cross over, mutation and replacement during each iteration of GA.
+/*
+	TODO add example
+*/
 func OptionWithPopulationFunc(fn PopulationFunc) Option {
 	return func(g *ga) error {
 		if fn != nil {
@@ -48,6 +51,9 @@ func OptionWithPopulationFunc(fn PopulationFunc) Option {
 }
 
 // OptionWithGeneratorFunc generator to make solutions randomly
+/*
+	TODO add example
+*/
 func OptionWithGeneratorFunc(fn func() Model) Option {
 	return func(g *ga) error {
 		if fn != nil {
@@ -59,6 +65,10 @@ func OptionWithGeneratorFunc(fn func() Model) Option {
 }
 
 // OptionWithSelection make subset of the population is selected for the next generation based on their fitness scores.
+// ex: the folowing options works the same.
+//  OptionWithSelection(0.2,0.6,0.2) 	// 20% top, 60% crossover, 20% random
+//  OptionWithSelection(20,60,20) 		// 20% top, 60% crossover, 20% random
+//  OptionWithSelection(1,3,1) 			// 20% top, 60% crossover, 20% random
 func OptionWithSelection(top, mutaion, random float64) Option {
 	return func(g *ga) error {
 		sum := top + mutaion + random
@@ -75,16 +85,13 @@ func OptionWithSelection(top, mutaion, random float64) Option {
 	}
 }
 
-// OptionWithReverse
-// todo add comment
-func OptionWithReverse(v bool) Option {
-	return func(g *ga) error {
-		return nil
-	}
-}
-
-// OptionWithStepInterval
-// todo add comment
+// OptionWithStepInterval is a function that returns an Option which sets the interval between two generations.
+// It is useful for runtime processes where you want to continuously improve the parameters, especially in cases where the cost function is not constant and changes over time.
+//
+// default: 0 (no wait)
+//
+// ex:
+//  OptionWithStepInterval(30 * time.Second)
 func OptionWithStepInterval(d time.Duration) Option {
 	return func(g *ga) error {
 		g.config.stepsInterval = d
@@ -92,8 +99,12 @@ func OptionWithStepInterval(d time.Duration) Option {
 	}
 }
 
-// OptionWithMaximumNumberOfSteps
-// todo add comment
+// OptionWithMaximumNumberOfSteps is a function that returns an Option which sets the maximum number of steps for a genetic algorithm.
+//
+// default: 1000
+//
+// ex:
+//  OptionWithMaximumNumberOfSteps(1000)
 func OptionWithMaximumNumberOfSteps(n int64) Option {
 	return func(g *ga) error {
 		g.config.maxNumOfSteps = n
@@ -101,9 +112,13 @@ func OptionWithMaximumNumberOfSteps(n int64) Option {
 	}
 }
 
-// OptionWithTargetCost
-// specify the target cost to when comuptation continues
-// todo add comment
+// OptionWithTargetCost is a function that returns an Option which sets the target cost for a genetic algorithm.
+// The target cost specifies the point at which the process will stop once it reaches that cost.
+//
+// default: 0.05
+//
+// ex:
+//  OptionWithTargetCost(0.2)
 func OptionWithTargetCost(v float64) Option {
 	return func(g *ga) error {
 		g.config.targetCost = v
@@ -111,10 +126,12 @@ func OptionWithTargetCost(v float64) Option {
 	}
 }
 
-// OptionWithInitialPopulation
-/*
- */
-//n number of the first population intitialization of randomly solutions
+// OptionWithInitialPopulation is a function that returns an Option that sets the initial population of the genetic algorithm.
+//
+// defaul: 1000
+//
+// ex:
+//  OptionWithInitialPopulation(10000)
 func OptionWithInitialPopulation(n uint64) Option {
 	return func(g *ga) error {
 		g.config.initialPopulation = n
